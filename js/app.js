5920bf89f74603ada0253d0576f1c1245c5c98bc
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-13 22:03:35 +1200

    Work in progress

--- js/app.js
+++ js/app.js
@@ -2,28 +2,15 @@ const select = document.getElementById('breeds');
 const card = document.querySelector('.card'); 
 const form = document.querySelector('form');
 
-// ------------------------------------------
-//  FETCH FUNCTIONS
-// ------------------------------------------
 
-fetch('https://dog.ceo/api/breeds/list')
-  .then(response => response.json())
-  .then(data => generateOptions(data.message))
+//  FETCH FUNCTIONS
 
 fetch('https://dog.ceo/api/breeds/image/random')
   .then(response => response.json())
   .then(data => generateImage(data.message))
 
-// ------------------------------------------
-//  HELPER FUNCTIONS
-// ------------------------------------------
 
-function generateOptions(data) {
-  const options = data.map(item => `
-    <option value='${item}'>${item}</option>
-  `).join('');
-  select.innerHTML = options;
-}
+//  HELPER FUNCTIONS
 
 function generateImage(data) {
   const html = `
@@ -33,13 +20,13 @@ function generateImage(data) {
   card.innerHTML = html;
 }
 
-// ------------------------------------------
+
 //  EVENT LISTENERS
-// ------------------------------------------
 
 
 
-// ------------------------------------------
+
+
 //  POST DATA
-// ------------------------------------------
+
 
