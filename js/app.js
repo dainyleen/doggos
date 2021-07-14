c532b3792e3e3935f850181f45f6401537eefcf8
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-14 21:08:30 +1200

    Work in progress

--- js/app.js
+++ js/app.js
@@ -2,28 +2,30 @@ const select = document.getElementById('breeds');
 const card = document.querySelector('.card'); 
 const form = document.querySelector('form');
 
-
+// ------------------------------------------
 //  FETCH FUNCTIONS
+// ------------------------------------------
 
 function fetchData(url) {
   return fetch(url)
-    .then(res => res.json())
+           .then(res => res.json())
 }
 
-fetch('https://dog.ceo/api/breeds/list')
+fetchData('https://dog.ceo/api/breeds/list')
   .then(data => generateOptions(data.message))
 
-fetch('https://dog.ceo/api/breeds/image/random')
+fetchData('https://dog.ceo/api/breeds/image/random')
   .then(data => generateImage(data.message))
 
-
+// ------------------------------------------
 //  HELPER FUNCTIONS
+// ------------------------------------------
 
 function generateOptions(data) {
   const options = data.map(item => `
     <option value='${item}'>${item}</option>
-  `).join('')
-  select.innerHTML = options
+  `).join('');
+  select.innerHTML = options;
 }
 
 function generateImage(data) {
@@ -35,25 +37,25 @@ function generateImage(data) {
 }
 
 function fetchBreedImage() {
-  const breed = select.value
-  const img = card.querySelector('img')
-  const p = card.querySelector('p')
-
-  fetchData(`'https://dog.ceo/api/breed/${breed}/images/random`)
+  const breed = select.value;
+  const img = card.querySelector('img');
+  const p = card.querySelector('p');
+  
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(data => {
-      img.src = data.message
-      img.alt = breed
-      p.textContent = `Click to view more ${breed}s`
+      img.src = data.message;
+      img.alt = breed;
+      p.textContent = `Click to view more ${breed}s`;
     })
 }
 
+// ------------------------------------------
 //  EVENT LISTENERS
+// ------------------------------------------
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
 
-select.addEventListener('change', fetchBreedImage)
-card.addEventListener('click', fetchBreedImage)
-
-
-
+// ------------------------------------------
 //  POST DATA
-
+// ------------------------------------------
 
