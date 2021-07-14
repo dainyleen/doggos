ca2dd0578b9d84da63236d34c816d9e55a2c93ab
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-14 21:16:16 +1200

    Created function checkStatus

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ const form = document.querySelector('form');
 function fetchData(url) {
   return fetch(url)
     .then(res => res.json())
+    .catch(error => console.log('Looks like there was a problem', error))
 }
 
 fetchData('https://dog.ceo/api/breeds/list')
   .then(data => generateOptions(data.message))
-  .catch(error => console.log('Looks like there was a problem', error))
 
 fetchData('https://dog.ceo/api/breeds/image/random')
   .then(data => generateImage(data.message))
@@ -21,6 +21,15 @@ fetchData('https://dog.ceo/api/breeds/image/random')
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
 
 function generateOptions(data) {
   const options = data.map(item => `
